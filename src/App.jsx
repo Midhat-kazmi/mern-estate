802ebc499f74d21dcaf886f2d0272de715228519
Midhat-dot <midhatcodes@gmail.com>
2025-01-14 03:24:53 -0800

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { Signin } from "./pages/Signin"
+import { Signup } from "./pages/Signup"
+import { Profile } from "./pages/profile"
+import { About } from "./pages/about"
 
 export const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-600">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/" element={< Home/>}/>
+      <Route path = "about" element={< About/>}/>
+      <Route path = "/profile" element={< Profile/>}/>
+      <Route path = "/sign-in" element={< Signin/>}/>
+      <Route path = "/sign-up" element={< Signup/>}/>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
