4f1a0c6b201cb40c06189083ff7c7a220be6a030
Midhat-dot <midhatcodes@gmail.com>
2025-01-15 21:44:51 -0800

    complete sign up page ui

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,3 +1,6 @@
+//custom error handler 
+// may be use sometimes in our project 
+
 export const errorHandler =(statusCode, message)=>{
    const error = new Error ()
    error.statusCode= statusCode
